201df6f4cc0d9f0f9998c7305240b287e766665b
patovera <verapatricio9@gmail.com>
2024-06-08 10:40:50 -0300

    feat: valid phone number

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,22 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+def valid_phone_number(phonenumber)
+  clean_number = phonenumber.to_s.gsub(' ', '').gsub('(', '').gsub(')', '').gsub('-', '')
+  if clean_number.length == 11 && clean_number[0] == '1'
+    real_number = clean_number.slice(1..10)
+  elsif clean_number.length == 10
+    real_number = clean_number
+  else 
+    real_number = '# N/a'
+  end
+  if real_number.length == 10
+    display_phonenumber = real_number.insert(6,'-').insert(3,') ').insert(0,'(')
+  else
+    display_phonenumber = real_number
+  end
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -46,6 +62,7 @@ contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  phone_number = valid_phone_number(row[:homephone])
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
