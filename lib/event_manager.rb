1ecdde4898bc7552018b182fd4e274c9d647a021
patovera <verapatricio9@gmail.com>
2024-06-08 10:52:12 -0300

    feat: finished

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,20 +6,81 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
-def valid_phone_number(phonenumber)
+def clean_phone_number(phonenumber)
   clean_number = phonenumber.to_s.gsub(' ', '').gsub('(', '').gsub(')', '').gsub('-', '')
   if clean_number.length == 11 && clean_number[0] == '1'
-    real_number = clean_number.slice(1..10)
+    ten_digit_number = clean_number.slice(1..10)
   elsif clean_number.length == 10
-    real_number = clean_number
+    ten_digit_number = clean_number
   else 
-    real_number = '# N/a'
+    ten_digit_number = '# N/a'
   end
-  if real_number.length == 10
-    display_phonenumber = real_number.insert(6,'-').insert(3,') ').insert(0,'(')
+  if ten_digit_number.length == 10
+    result_number = ten_digit_number.insert(6,'-').insert(3,') ').insert(0,'(')
   else
-    display_phonenumber = real_number
+    result_number = ten_digit_number
   end
+end
+
+def day_of_week(hashkey)
+  case hashkey
+  when '0'
+    'Sunday'
+  when '1'
+    'Monday'
+  when '2'
+    'Tuesday'
+  when '3'
+    'Wednesday'
+  when '4'
+    'Thursday'
+  when '5'
+    'Friday'
+  when'6'
+    'Saturday'
+  end
+end
+
+
+def most_common_day
+  contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+  common_day = Hash.new 
+  contents.each do |row|
+    date = DateTime.strptime(row[:regdate], '%D %R')
+    wday = date.wday
+    if common_day.include?(wday) == false
+      common_day[wday] = 1
+    else
+      common_day[wday] += 1
+    end
+  end
+  most_common_day = common_day.key(common_day.values.sort.slice(0))
+  p most_common_day
+  day_of_week(most_common_day.to_s)
+end
+
+def most_common_hour
+  contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+  common_hours = Hash.new
+  contents.each do |row|
+    date = DateTime.strptime(row[:regdate], '%D %R')
+    hour = date.hour
+    if common_hours.include?(hour) == false
+      common_hours[hour] = 1
+    else
+      common_hours[hour] += 1
+    end
+  end
+    most_common_hour = common_hours.key(common_hours.values.sort.slice(0))
+    return most_common_hour
 end
 
 def legislators_by_zipcode(zip)
@@ -62,7 +123,7 @@ contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-  phone_number = valid_phone_number(row[:homephone])
+  phone_number = clean_phone_number(row[:homephone])
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
